80d416f423b4702430a66b9c7004a653e8e352e8
Nakhun Song <nak4627@gmail.com>
2019-11-14 23:56:13 +0900

    init

---
+++ src/Root.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './base.css';
+
+const Root = () => {
+    return (
+        <div>
+            <div className="header">
+                React Header
+            </div>
+            <div>
+                React Body
+            </div>
+        </div>
+    );
+};
+
+export default Root;
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname + '/dist'),
+    },
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.json', '.jsx', '.css']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        [
+                            '@babel/preset-env', {
+                                targets: { node: 'current' },
+                                modules: 'false'
+                            }
+                        ],
+                        '@babel/preset-react'
+                    ],
+                },
+                exclude: ['/node_modules'],
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+              test: /\.css$/,
+              use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+          filename: 'style.css'
+        }),
+        new webpack.EnvironmentPlugin(['NODE_ENV']),
+    ]
+}
